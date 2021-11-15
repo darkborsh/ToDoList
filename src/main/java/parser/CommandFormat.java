8bdb8b1725e549c9824269ef6fa1057e8075f7f5
darkborsh <n_ragnar@mail.ru>
2021-11-16 02:59:46 +0300

    Изменил класс InputProcessor по материалам из разбора дз.

--- src/main/java/parser/CommandFormat.java
+++ src/main/java/parser/CommandFormat.java
@@ -5,8 +5,9 @@ import lombok.Getter;
 
 @Builder
 @Getter
-public class CommandDescription {
+public class CommandFormat {
     private final String name;
+    private final String args;
     private final long id;
     private final String text;
 }
---
+++ src/main/java/parser/InputProcessor.java
@@ -0,0 +1,27 @@
+package parser;
+
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputProcessor implements Function<String, CommandFormat> {
+    private final Pattern COMMAND_FORMAT =
+            Pattern.compile("\\s*(?<cmd>\\w+)(?:\\s+(?<args>(?:(?<id>\\d+)\\b)?(?<text>.*)))?");
+
+    @Override
+    public CommandFormat apply(String userInput) {
+        Matcher cmdMatcher = COMMAND_FORMAT.matcher(userInput);
+        if (cmdMatcher.find()) {
+            CommandFormat.CommandFormatBuilder builder = CommandFormat.builder()
+                    .name(cmdMatcher.group("cmd"))
+                    .args(cmdMatcher.group("args"))
+                    .text(cmdMatcher.group("text"));
+            String taskId = cmdMatcher.group("id");
+            if (taskId != null) {
+                builder.id(Long.parseLong(taskId));
+            }
+            return builder.build();
+        }
+        return null;
+    }
+}
