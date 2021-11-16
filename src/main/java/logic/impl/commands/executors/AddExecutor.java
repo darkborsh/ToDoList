f9f644553ca5be263b9466c778fd87b7e48c3984
darkborsh <n_ragnar@mail.ru>
2021-11-16 11:32:35 +0300

    Добавил класс AddExecutor.

---
+++ src/main/java/logic/impl/commands/executors/AddExecutor.java
@@ -0,0 +1,14 @@
+package logic.impl.commands.executors;
+
+import logic.TaskDao;
+import model.Task;
+import parser.CommandFormat;
+
+import java.util.function.BiConsumer;
+
+public class AddExecutor implements BiConsumer<CommandFormat, TaskDao> {
+    @Override
+    public void accept(CommandFormat commandFormat, TaskDao taskDao) {
+        taskDao.save(new Task(commandFormat.getArgs()));
+    }
+}
