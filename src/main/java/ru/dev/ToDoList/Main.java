fbec8903f12998df9e1eb6578f5d6a9ce0d98ef3
darkborsh <n_ragnar@mail.ru>
2021-11-22 19:25:54 +0300

    Убрал лишние зависимости в Main.

--- src/main/java/ru/dev/ToDoList/Main.java
+++ src/main/java/ru/dev/ToDoList/Main.java
@@ -5,11 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 
-import ru.dev.ToDoList.logic.TaskDao;
 import ru.dev.ToDoList.logic.impl.commands.CommandConsumer;
-import ru.dev.ToDoList.logic.impl.commands.StaticCommandFactory;
 import ru.dev.ToDoList.model.CommandFormat;
-import ru.dev.ToDoList.presenters.ErrorHandler;
 import ru.dev.ToDoList.presenters.impl.parser.InputProcessor;
 
 import java.io.BufferedReader;
@@ -20,19 +17,12 @@ import java.util.function.Consumer;
 public class Main implements CommandLineRunner {
     final BufferedReader reader;
     final InputProcessor inputProcessor;
-    final ErrorHandler errorHandler;
-    final TaskDao taskDao;
-    final StaticCommandFactory commandFactory;
     final Consumer<CommandFormat> consumer;
 
     @Autowired
-    public Main(BufferedReader reader, InputProcessor inputProcessor, ErrorHandler errorHandler, TaskDao taskDao,
-                StaticCommandFactory commandFactory, CommandConsumer consumer) {
+    public Main(BufferedReader reader, InputProcessor inputProcessor, CommandConsumer consumer) {
         this.reader = reader;
         this.inputProcessor = inputProcessor;
-        this.errorHandler = errorHandler;
-        this.taskDao = taskDao;
-        this.commandFactory = commandFactory;
         this.consumer = consumer;
     }
 
