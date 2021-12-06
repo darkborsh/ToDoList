efca2978fe2e66c90a3687c893b6a93105ed6fe6
darkborsh <n_ragnar@mail.ru>
2021-12-06 18:40:38 +0300

    Убрал конфликты.

--- src/main/java/ru/dev/ToDoList/controllers/TasksController.java
+++ src/main/java/ru/dev/ToDoList/controllers/TasksController.java
@@ -1,7 +1,6 @@
 package ru.dev.ToDoList.controllers;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.dev.ToDoList.dao.TaskDao;
