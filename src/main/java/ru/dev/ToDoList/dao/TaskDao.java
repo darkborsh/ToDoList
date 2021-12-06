c2dcd990cbc48699ea1608fbdbd4bfc9df626d25
darkborsh <n_ragnar@mail.ru>
2021-12-06 17:45:52 +0300

    Добавил аннотацию @Repository к TaskDao.

--- src/main/java/ru/dev/ToDoList/dao/TaskDao.java
+++ src/main/java/ru/dev/ToDoList/dao/TaskDao.java
@@ -1,10 +1,12 @@
 package ru.dev.ToDoList.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.dev.ToDoList.model.Task;
 
 import java.util.List;
 
+@Repository
 public interface TaskDao extends JpaRepository<Task, Long> {
     List<Task> findAllByIsCompleted(boolean isCompleted);
     List<Task> findAllByDescriptionIsLike(String substring);
