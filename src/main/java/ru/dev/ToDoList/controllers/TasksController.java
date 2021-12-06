bf73b51b2c74fbc2fc9fcba7f6bebfeac1da5794
darkborsh <n_ragnar@mail.ru>
2021-12-06 08:25:12 +0300

    Добавил swagger.

--- src/main/java/ru/dev/ToDoList/controllers/TasksController.java
+++ src/main/java/ru/dev/ToDoList/controllers/TasksController.java
@@ -1,8 +1,6 @@
-package ru.dev.ToDoList.contollers;
+package ru.dev.ToDoList.controllers;
 
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
