b7c12e916b6de79bcda1a110c33f2d970fd0396d
darkborsh <n_ragnar@mail.ru>
2021-12-14 15:08:35 +0300

    Добавил UserMapper и UserDto.

---
+++ src/main/java/ru/dev/ToDoList/dto/mappers/UserMapper.java
@@ -0,0 +1,16 @@
+package ru.dev.ToDoList.dto.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import ru.dev.ToDoList.dto.UserDto;
+import ru.dev.ToDoList.model.User;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper( UserMapper.class );
+
+    public UserDto toDto(User task);
+    public List<UserDto> toDtoList (List<User> taskList);
+}
