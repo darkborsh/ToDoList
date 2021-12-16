1dbec03a73d2a6cdfe8f79c1c1efd61488f769ab
darkborsh <n_ragnar@mail.ru>
2021-12-16 13:15:41 +0300

    Удалил бесполезный метод.

--- src/main/java/ru/dev/ToDoList/dao/UserDao.java
+++ src/main/java/ru/dev/ToDoList/dao/UserDao.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserDao extends JpaRepository<User, Long> {
-    Optional<User> findUserById(long id);
     Optional<User> findByName(String username);
 }
